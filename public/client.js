7612089096e85fe333eeab4ca2033fd2a8c881be
Suman <suman.dey.ece24@heritageit.edu.in>
2024-06-11 15:02:22 +0530

    final may be

--- public/client.js
+++ public/client.js
@@ -1,5 +1,5 @@
 // const socket = io('http://localhost:5000/') 
-const socket = io('https://realtime-chat-app-tau-green.vercel.app:5000/')
+const socket = io('https://realtime-chat-app-tau-green.vercel.app')
 // const socket = io('https://sumanschatapp.vercel.app')
 
 // const socket = io({
