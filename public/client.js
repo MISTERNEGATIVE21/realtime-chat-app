c0fdfc86e0814d2ccdc8bfab5631e20476f9a964
Suman <suman.dey.ece24@heritageit.edu.in>
2024-06-12 23:12:12 +0530

    trying

--- public/client.js
+++ public/client.js
@@ -1,7 +1,8 @@
 // const socket = io('http://localhost:5000/') 
 //const socket = io('http://192.168.1.106:5000') 
 //const socket = io('https://realtime-chat-app-tau-green.vercel.app')
-const socket = io('https://sumanschatapp.vercel.app')
+// const socket = io('https://sumanschatapp.vercel.app')
+const socket = io();
 
 // const socket = io({
 //     transports: ['http'],
